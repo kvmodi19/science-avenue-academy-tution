d09417023e713b759014778cc8c20ea7d1bbf31f
Kinjal Modi <kvmodi19@gmail.com>
2023-03-19 14:41:30 +0530

    added assetsPrefix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
   basePath: 'https://kvmodi19.github.io/science-avenue-academy-tution',
+  assetPrefix: '/science-avenue-academy-tution/',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
       new webpack.ProvidePlugin({
