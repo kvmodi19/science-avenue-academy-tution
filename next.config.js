4917ded963dc2903206ab3cdedcf1f9ff36f9f6e
Kinjal Modi <kvmodi19@gmail.com>
2023-03-20 22:45:00 +0530

    reverted next config

--- next.config.js
+++ next.config.js
@@ -1,35 +1,29 @@
 /** @type {import('next').NextConfig} */
-const {
-  PHASE_PRODUCTION_BUILD,
-} = require('next/constants');
+const webpack = require("webpack");
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = (phase) => {
-  // when `next build` or `npm run build` is used
-  const isProd = phase === PHASE_PRODUCTION_BUILD
+const nextConfig = {
+  reactStrictMode: true,
+  // basePath:'/science-avenue-academy-tution',
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    // staticFolder: isProd ? '/science-avenue-academy-tution' : '',
+    staticFolder: '/science-avenue-academy-tution',
+  },
+  images:{
+    unoptimized: true
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      }));
+    return config;
 
-  const env = {
-    reactStrictMode: true,
-    publicRuntimeConfig: {
-      // Will be available on both server and client
-      staticFolder: isProd ? '/science-avenue-academy-tution' : '',
-    },
-    images: {
-      unoptimized: true
-    },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery",
-          "window.jQuery": "jquery",
-        }));
-      return config;
-
-    }
-  }
-
-  // next.config.js object
-  return {
-    ...env,
   }
 }
+
+
+module.exports = nextConfig
