b5600f036e78adafa29cbfc53f0acfdff9874bf6
Kinjal Modi <kvmodi19@gmail.com>
2023-03-20 15:57:11 +0530

    fix build error

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const {
-  PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
 } = require('next/constants');
 
 module.exports = (phase) => {
-  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environment variable
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
   // when `next build` or `npm run build` is used
   const isProd = phase === PHASE_PRODUCTION_BUILD
-
-  console.log(`isDev:${isDev}  isProd:${isProd} `)
 
   const env = {
     reactStrictMode: true,
@@ -35,6 +30,6 @@ module.exports = (phase) => {
 
   // next.config.js object
   return {
-    env,
+    ...env,
   }
 }
