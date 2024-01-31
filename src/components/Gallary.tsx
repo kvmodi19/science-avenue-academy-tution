1384e5df771e60a76195f2e0bc98c8150a2d7c79
Kinjal Modi <kvmodi19@gmail.com>
2024-01-31 21:05:55 +0530

    error fix

--- src/components/Gallary.tsx
+++ src/components/Gallary.tsx
@@ -99,17 +99,3 @@ export default function Gallary() {
     )
 }
 
-async function getDoument(collection, id) {
-    let docRef = doc(db, collection, id);
-
-    let result = null;
-    let error = null;
-
-    try {
-        result = await getDoc(docRef);
-    } catch (e) {
-        error = e;
-    }
-
-    return { result, error };
-}
